f01667363d496f89e60e277c93f5b89d69f7c97d
R-koma <ryomakomazawa@gmail.com>
2026-01-31 00:56:58 +0900

    feature: learn new way to write for sentence

--- 01-basics/for/main.go
+++ 01-basics/for/main.go
@@ -7,9 +7,16 @@ Goに、whileはない
 */
 
 func main() {
-	res := 0
-	for i := 0; i <= 100; i++ {
-		res += i
+	res1 := 0
+	for i := 0; i < 100; i++ {
+		res1 += i
 	}
-	fmt.Println(res)
+	fmt.Println(res1)
+
+	// Go 1.22以降の記述方法
+	res2 := 0
+	for j := range 100 {
+		res2 += j
+	}
+	fmt.Println(res2)
 }
