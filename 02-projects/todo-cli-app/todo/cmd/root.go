bf2d49099ddd48931134224761a50cfcb62a57ec
R-koma <ryomakomazawa@gmail.com>
2026-02-03 13:58:27 +0900

    feat: integrate Viper for 12-factor app configuration
      - Add Viper and Cobra dependencies
      - Implement config priority: flags > env vars > file > defaults
      - Create serve command with configurable port
      - Add config.yaml for file-based settings

--- 02-projects/todo-cli-app/todo/cmd/root.go
+++ 02-projects/todo-cli-app/todo/cmd/root.go
@@ -1,31 +1,44 @@
 /*
 Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"errors"
+	"fmt"
 	"os"
+	"strings"
 
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "todo-cli-app",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
+// var rootCmd = &cobra.Command{
+// 	Use:   "todo-cli-app",
+// 	Short: "A brief description of your application",
+// 	Long: `A longer description that spans multiple lines and likely contains
+// examples and usage of using your application. For example:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
-}
+// Cobra is a CLI library for Go that empowers applications.
+// This application is a tool to generate the needed files
+// to quickly create a Cobra application.`,
+//
+//		// Uncomment the following line if your bare application
+//		// has an action associated with it:
+//		// Run: func(cmd *cobra.Command, args []string) { },
+//	}
+var (
+	cfgFile string
+
+	rootCmd = &cobra.Command{
+		Use:   "todo",
+		Short: "todo app with Cobra and Viper",
+		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+			return initializeConfig(cmd)
+		},
+	}
+)
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
@@ -41,11 +54,41 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.todo-cli-app.yaml)")
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.todo-cli-app.yaml)")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
+func initializeConfig(cmd *cobra.Command) error {
+	viper.SetEnvPrefix("todo")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_", "-", "_"))
+	viper.AutomaticEnv()
 
+	if cfgFile != "" {
+		viper.SetConfigFile(cfgFile)
+	} else {
+		home, err := os.UserHomeDir()
+		cobra.CheckErr(err)
+		viper.AddConfigPath(".")
+		viper.AddConfigPath(home + "/.todo")
+		viper.SetConfigName("config")
+		viper.SetConfigType("yaml")
+	}
+
+	if err := viper.ReadInConfig(); err != nil {
+		var configFileNotFoundError viper.ConfigFileNotFoundError
+		if !errors.As(err, &configFileNotFoundError) {
+			return err
+		}
+	}
+
+	err := viper.BindPFlags(cmd.Flags())
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("Configuration initialized. Using config file:", viper.ConfigFileUsed())
+	return nil
+}
